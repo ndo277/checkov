7485e66b4ed6a9ee6dfb270ed8f594f5ed045319
ndo277 <nhat.do277@gmail.com>
2019-08-05 15:28:47 -0700

    Add & test session api utility

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import {signup, login, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  /**
+   * For Testing Purposes
+   */
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+  /**
+   * 
+   */
+
 
   ReactDOM.render(<Root/>, root);
 });
