ca74618fed0cdfaca2aab62af64f4dc5752a9d1f
ndo277 <nhat.do277@gmail.com>
2019-08-06 21:57:38 -0700

    Add session actions & users reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,45 @@
+import * as ApiUtil from '../util/session_api_util.js';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const CLEAR_SESSION_ERRORS = 'CLEAR_SESSION_ERRORS';
+
+const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS,
+  errors: []
+});
+
+export const login = (user) => (dispatch) => {
+  return ApiUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON)));
+};
+
+export const signup = (user) => (dispatch) => (
+  ApiUtil.signup(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const logout = () => (dispatch) => {
+  return ApiUtil.logout().then(
+    user => dispatch(logoutCurrentUser()),
+    err => dispatch(receiveErrors(err.responseJSON)));
+};
