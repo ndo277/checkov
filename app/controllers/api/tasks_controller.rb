4c7043adb767367e13e239b09d518c950b89cdf9
ndo277 <nhat.do277@gmail.com>
2019-08-07 20:47:56 -0700

    Add task routes & controller

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,3 @@
+class Api::TasksController < ApplicationController
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
+    resources :tasks, only: [:index, :show, :create, :update, :destroy]
   end
 
-  root to: 'static_pages#root'  
+  root to: 'static_pages#root' 
 end
