a047d091903ebd4c44961d82728e7df8383a4b81
ndo277 <nhat.do277@gmail.com>
2019-08-07 19:15:27 -0700

    Add auth & protected routing

---
+++ frontend/components/all_tasks.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AllTasks() {
+  return(
+    <marquee behavior="" direction="">UNDER CONSTRUCTION</marquee>
+  )
+}
+
+export default AllTasks;
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Splash from './splash';
+import AllTasks from './all_tasks';
 
 function App(){
   return(
    <div>
-     <Splash />
+     <AuthRoute exact path="/" component={Splash} />
+     <ProtectedRoute path="/all" component={AllTasks} />
    </div>
   )
 }
