e30b7f9005ea42601df25539cd88990546ec3e00
ndo277 <nhat.do277@gmail.com>
2019-08-07 20:29:09 -0700

    Migrate tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_172345) do
+ActiveRecord::Schema.define(version: 2019_08_08_031933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "body", null: false
+    t.boolean "checked", default: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
