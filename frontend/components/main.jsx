572314525dbfe1d0f42b3a5ed6d98b4661ca52db
ndo277 <nhat.do277@gmail.com>
2019-08-09 16:59:05 -0700

    Add task index & form to main

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Main(props) {
+
+  useEffect(() => {
+    props.fetchTasks();
+  }, []);
+
+  const [task, setTask] = useState("");
 
   const handleLogOut = () => {
     props.logout();
   };
 
+  const handleTaskSubmit = () => {
+    const taskData = {body: task};
+    props.createTask(taskData).then(() => {
+      setTask("");
+    });
+  };
+
+  const handleTaskInput = (e) => {
+    setTask(e.currentTarget.value);
+  };
+
   return(
-    <div className="header">
-      <h3>CHECKOV</h3>
-      <p>Hi, {props.currentUser.username}.</p>
-      <button onClick={handleLogOut}>Log Out</button>
-    </div>
+    <div>
+      <div className="header">
+        <h3>CHECKOV</h3>
+        <p>Hi, {props.currentUser.username}.</p>
+        <button onClick={handleLogOut}>Log Out</button>
+      </div>
+
+      <div>
+        <h1>TASKS</h1>
+
+        <form onSubmit={handleTaskSubmit}>
+          <input type="text" onChange={handleTaskInput} value={task} />
+          <input type="submit" />
+        </form>
+
+        {props.tasks.map(task => {
+          return <li key={task.id}>
+            {task.body}
+          </li>
+        })}
+      </div>
+      
+    </div> 
   )
 }
 
--- frontend/components/main_container.js
+++ frontend/components/main_container.js
@@ -1,13 +1,19 @@
 import { connect } from 'react-redux';
 import Main from './main';
 import { logout } from '../actions/session_actions';
+import { fetchTasks, createTask} from '../actions/task_actions';
 
-const mapStateToProps = (state) => ({
-  currentUser: state.entities.users[state.session.id]
-});
+const mapStateToProps = (state) => {
+  const currentUser = state.entities.users[state.session.id];
+  const tasks = Object.values(state.entities.tasks);
+
+  return {currentUser, tasks};
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logout())
+  logout: () => dispatch(logout()),
+  fetchTasks: () => dispatch(fetchTasks()),
+  createTask: (data) => dispatch(createTask(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
